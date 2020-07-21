5101405cd0b6d33c3ea37ba6236cf1dcf7249836
bonbon0202 <bonbon0202@gmail.com>
2020-07-21 19:46:39 +0900

    Refactor: 멘토 리뷰 반영

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { iconGiftBox } from "../../config";
+import { iconKaKao } from "../../config";
 import "./Login.scss";
 
 class Login extends React.Component {
@@ -21,14 +23,12 @@ class Login extends React.Component {
     }
   };
 
-  handleId = (e) => {
-    this.state.userId.includes("@")
-      ? this.setState({ incorrectId: false })
-      : this.setState({ incorrectId: true });
+  handleId = () => {
+    const { userId } = this.state;
+    this.setState({ incorrectId: userId.includes("@") ? false : true });
   };
 
-  handlePw = (e) => {
-    console.log(this.state.userPw);
+  handlePw = () => {
     const numbers = /[0-9]/;
     const spellings = /[a-zA-Z]/;
     const specialCharacters = /[~!@#$%<>^&*]/;
@@ -52,17 +52,19 @@ class Login extends React.Component {
     }
   };
 
-  handleLogin = (e) => {
+  handleLogin = () => {
     const { userId, userPw } = this.state;
-    fetch("http://10.58.0.114:8000/user/signin", {
-      method: "POST",
-      body: JSON.stringify({
-        email: userId,
-        password: userPw,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => this.props.history.push("/"));
+    if (userId && userPw) {
+      fetch("http://10.58.0.114:8000/user/signin", {
+        method: "POST",
+        body: JSON.stringify({
+          email: userId,
+          password: userPw,
+        }),
+      })
+        .then((res) => res.json())
+        .then((res) => this.props.history.push("/"));
+    }
   };
 
   render() {
@@ -108,7 +110,7 @@ class Login extends React.Component {
           </button>
           <button className="kakaoLoginBtn">
             <svg id="icon-kakao" viewBox="0 0 24 22">
-              <path d="M12,0C5.373,0,0,4.238,0,9.465c0,3.403,2.277,6.386,5.693,8.054 c-0.251,0.935-0.909,3.387-1.04,3.912c-0.163,0.65,0.239,0.643,0.502,0.467c0.206-0.138,3.288-2.229,4.618-3.13 c0.722,0.106,1.466,0.163,2.227,0.163c6.627,0,12-4.238,12-9.466C24,4.238,18.627,0,12,0"></path>
+              <path d={iconKaKao}></path>
             </svg>
             <p>카카오 계정으로 1초 로그인</p>
           </button>
@@ -118,7 +120,7 @@ class Login extends React.Component {
           </div>
           <div className="membershipBenefit">
             <svg id="icon-giftbox" viewBox="0 0 32 32">
-              <path d="M24.96,6.197c0.607-0.653,0.986-1.541,0.986-2.521C25.946,1.632,24.313,0,22.335,0 c-1.502,0-4.964,2.246-6.348,4.264c-1.43-2.018-4.879-4.199-6.321-4.199c-1.973,0-3.612,1.633-3.612,3.677 c0,0.946,0.353,1.802,0.921,2.448H0v8.784h1.907V32h10.012h8.169H30.1V14.981H32V6.197H24.96z M22.335,1.365 c1.228,0,2.246,1.018,2.246,2.383c0,1.149-0.803,2.136-1.842,2.345h-5.524c-0.353-0.105-0.464-0.229-0.464-0.301 C16.751,4.63,20.833,1.365,22.335,1.365 M9.666,1.365c1.43,0,5.583,3.265,5.583,4.427c0,0.105-0.242,0.222-0.418,0.301H9.267 C8.229,5.884,7.425,4.897,7.425,3.749C7.419,2.449,8.444,1.365,9.666,1.365 M11.912,30.636H3.266V14.981h8.646V30.636z M11.912,13.688H1.365V7.563h10.554v6.126H11.912z M18.717,30.636H13.27V7.557h5.447V30.636z M28.729,30.636h-8.646V14.981h8.646 V30.636z M30.629,13.688H20.082V7.563h10.553v6.126H30.629z"></path>
+              <path d={iconGiftBox}></path>
             </svg>
             지금 회원으로 가입하시고, 특별한 멤버십 혜택과 <br></br>다양한 회원
             전용 상품을 만나보세요.
