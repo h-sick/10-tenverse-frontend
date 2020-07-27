753e5a81cd3d242681abea884b9941bda67704fa
bonbon0202 <bonbon0202@gmail.com>
2020-07-27 16:19:36 +0900

    수정중

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,52 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MdClose } from "react-icons/md";
+import { AiOutlineClose } from "react-icons/ai";
 import "./SideBar.scss";
 
+const navDatas = [
+  { title: "전체보기", url: "/category/shoes" },
+  { title: "척테일러 올스타", url: "/category/chucktaylorallstar" },
+  { title: "척 70", url: "/category/chuck70" },
+  { title: "원스타", url: "/category/onestar" },
+  { title: "잭퍼셀", url: "/category/jackpurcell" },
+  { title: "프로레더", url: "/category/proleather" },
+];
+
+const recommendDatas = [
+  {
+    title: "빅 사이즈",
+    url:
+      "https://image.converse.co.kr/cmsstatic/menu/12723/Flyout_256x300_01-1.jpg",
+  },
+  {
+    title: "농구화 컬렉션",
+    url:
+      "https://image.converse.co.kr/cmsstatic/menu/12733/Flyout_256x300_03-1.jpg",
+  },
+  {
+    title: "크리에이트 더 퓨처",
+    url:
+      "https://image.converse.co.kr/cmsstatic/menu/12751/Flyout_256x300_02-1.jpg",
+  },
+  {
+    title: "트라이블록",
+    url:
+      "https://image.converse.co.kr/cmsstatic/menu/12752/Flyout_256x300_04-1.jpg",
+  },
+];
+
 class SideBar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      navDatas: [
-        { title: "전체보기", url: "/category/shoes" },
-        { title: "척테일러 올스타", url: "/category/chucktaylorallstar" },
-        { title: "척 70", url: "/category/chuck70" },
-        { title: "원스타", url: "/category/onestar" },
-        { title: "잭퍼셀", url: "/category/jackpurcell" },
-        { title: "프로레더", url: "/category/proleather" },
-      ],
-      recommendDatas: [
-        {
-          title: "빅 사이즈",
-          url:
-            "https://image.converse.co.kr/cmsstatic/menu/12723/Flyout_256x300_01-1.jpg",
-        },
-        {
-          title: "농구화 컬렉션",
-          url:
-            "https://image.converse.co.kr/cmsstatic/menu/12733/Flyout_256x300_03-1.jpg",
-        },
-        {
-          title: "크리에이트 더 퓨처",
-          url:
-            "https://image.converse.co.kr/cmsstatic/menu/12751/Flyout_256x300_02-1.jpg",
-        },
-        {
-          title: "트라이블록",
-          url:
-            "https://image.converse.co.kr/cmsstatic/menu/12752/Flyout_256x300_04-1.jpg",
-        },
-      ],
-    };
-  }
   render() {
-    const { navDatas, recommendDatas } = this.state;
     const { sideBarDisplay } = this.props;
     console.log(sideBarDisplay);
     return (
       <div className={sideBarDisplay ? "SideBar" : "HiddenSideBar"}>
         <div className="sideBarOverlay">
-          <div className="positionBox">
+          <div className="iconBox">
+            <AiOutlineClose className="closeIcon" />
+          </div>
+          {/* <div className="positionBox">
             <svg
               id="icon-close"
               viewBox="0 0 37 37"
@@ -57,7 +56,7 @@ class SideBar extends React.Component {
                 d="M36.533 3.533L33 0 18.267 14.733 3.533 0 0 3.533l14.733 14.734L0 33l3.533 3.533L18.267 21.8 33 36.533 36.533 33 21.8 18.267z"
               ></path>
             </svg>
-          </div>
+          </div> */}
         </div>
         <div className="sideBarInner">
           <main className="main">
