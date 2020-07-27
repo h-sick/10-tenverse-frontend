71f59ca520920aa258eb570f6a7e5c29e3382917
bonbon0202 <bonbon0202@gmail.com>
2020-07-27 17:36:20 +0900

    Add: sideBar UI 및 링크 구현

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -40,25 +40,11 @@ class SideBar extends React.Component {
     const { sideBarDisplay } = this.props;
     console.log(sideBarDisplay);
     return (
-      <div className={sideBarDisplay ? "SideBar" : "HiddenSideBar"}>
-        <div className="sideBarOverlay">
-          <div className="iconBox">
-            <AiOutlineClose className="closeIcon" />
-          </div>
-          {/* <div className="positionBox">
-            <svg
-              id="icon-close"
-              viewBox="0 0 37 37"
-              onClick={this.props.handleSideBar}
-            >
-              <path
-                fill-rule="nonzero"
-                d="M36.533 3.533L33 0 18.267 14.733 3.533 0 0 3.533l14.733 14.734L0 33l3.533 3.533L18.267 21.8 33 36.533 36.533 33 21.8 18.267z"
-              ></path>
-            </svg>
-          </div> */}
-        </div>
-        <div className="sideBarInner">
+      <div className="SideBar">
+        <div
+          className={`sideBarOverlay ${sideBarDisplay ? "" : "hidden"}`}
+        ></div>
+        <div className={`sideBarInner ${sideBarDisplay ? "" : "hidden"}`}>
           <main className="main">
             <ul>
               {navDatas.map((navData) => {
